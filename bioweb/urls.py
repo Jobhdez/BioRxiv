a54c5686b4ac8c2259d4aa427fdc4386476155b2
Job Hernandez <hj93@protonmail.com>
2021-10-18 13:53:32 -0700

    created api (PaperList).

--- bioweb/urls.py
+++ bioweb/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bioarxiv/', include('bioarxiv.urls', namespace='bioarxiv')),
+    path('api/', include('api.urls', namespace='api')),
 ]
